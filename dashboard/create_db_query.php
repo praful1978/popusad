740e82f9812dda29dc16784929bb38941d38a370
dsoyavatmal <shreetech15@gmail.com>
2024-08-12 23:02:42 +0530

    ff

--- dashboard/create_db_query.php
+++ dashboard/create_db_query.php
@@ -1,16 +1,12 @@
 <?php
 
-// $servername = "localhost";  // or your server
-// $username = "root";         // your database username
-// $password = "";             // your database password
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "localhost";  // or your server
-$username = "popusad";      // your database username
-$password = "Gl(^BWI}5I0L"; // your database password
+$servername = "localhost";
+$username = "popusad";
+$password = "Gl(^BWI}5I0L";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -23,14 +19,14 @@ if ($conn->connect_error) {
 // Sanitize user input
 $database_name = isset($_POST['database_name']) ? $_POST['database_name'] : '';
 $database_year = isset($_POST['database_year']) ? $_POST['database_year'] : '';
-$database = $database_name . " " . $database_year;
+$database = $database_name . "_" . $database_year; // Adjust to avoid spaces
 
 // Escape the database name to prevent SQL injection
 $database = $conn->real_escape_string($database);
 
 // Create database
 $sql = "CREATE DATABASE `$database`";
-
+echo $sql;
 if ($conn->query($sql) === TRUE) {
     echo '<script>alert("Database Created Successfully")</script>';
 } else {
